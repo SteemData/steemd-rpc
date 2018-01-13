f78e8aa25dbb220ba8bfcb666328c62a7de51f61
furion <_@furion.me>
2018-01-13 10:41:48 +0100

    Updated benchmarks
    * Use requests.Session for connection pooling
    * Import and modify jussi's benchmarking tool

--- benchmark/bench.py
+++ benchmark/bench.py
@@ -7,6 +7,8 @@ from contextlib import contextmanager, suppress
 import click
 import requests
 
+session = requests.Session()
+
 
 @contextmanager
 def timeit():
@@ -26,7 +28,7 @@ def get_random_block_body():
 
 def get_random_block(steemd_url):
     with suppress(Exception):
-        return requests.post(
+        return session.post(
             steemd_url,
             data=get_random_block_body()).text
 
@@ -35,17 +37,15 @@ def benchmark_get_blocks(steemd_url, num_of_requests, max_workers=100):
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
         futures = (executor.submit(get_random_block, steemd_url)
                    for _ in range(num_of_requests))
-        for future in as_completed(futures):
-            pass
-            # res = future.result()
-            # print(res)
+        for _ in as_completed(futures):
+            continue
 
 
 @click.command()
 @click.argument('node-url', default="https://api.steemit.com")
-@click.argument('num-of-requests', default=100)
+@click.argument('num-of-requests', default=1000)
 def benchmark(node_url, num_of_requests):
-    print("Getting %d blocks from %s" % (num_of_requests, node_url))
+    print("Getting %d random blocks from %s" % (num_of_requests, node_url))
     with timeit():
         benchmark_get_blocks(node_url, num_of_requests)
 
