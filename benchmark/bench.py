d361a060db27e8dab8c690200b8f360da8c88ec1
user <_@furion.me>
2018-01-15 21:08:19 +0100

    Add auto benchmark mode

--- benchmark/bench.py
+++ benchmark/bench.py
@@ -3,6 +3,7 @@ import random
 import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from contextlib import contextmanager, suppress
+from prettytable import PrettyTable
 
 import click
 import requests
@@ -80,7 +81,7 @@ def benchmark_blocks(ctx, node_url, num_of_requests):
 @click.argument('node-url', default="https://api.steemit.com")
 @click.argument('num-of-requests', default=1000)
 @click.pass_context
-def benchmark_blocks(ctx, node_url, num_of_requests):
+def benchmark_vops(ctx, node_url, num_of_requests):
     print("Getting %d random virtual-blocks from %s" % (num_of_requests, node_url))
     request_bodies = [
         get_random_vops_body() for _ in range(num_of_requests)
@@ -91,6 +92,37 @@ def benchmark_blocks(ctx, node_url, num_of_requests):
             request_bodies,
             max_workers=ctx.obj['threads'])
 
+@cli.command('auto')
+@click.argument('node-url', default="https://api.steemit.com")
+@click.argument('num-of-requests', default=10000)
+def benchmark_auto(node_url, num_of_requests):
+    t = PrettyTable(["Host", "Bench", "Requests", "Threads", "Time"])
+    t.align = "l"
+
+    for bench_fn in [get_random_block_body, get_random_vops_body]:
+        for threads in [10, 50, 100, 250, 500, 1000]:
+            click.echo("Making %d requests to %s [%s threads]"
+                % (num_of_requests, node_url, threads))
+            request_bodies = [
+                bench_fn() for _ in range(num_of_requests)
+            ]
+            start = time.time()
+            run_benchmark(
+                node_url,
+                request_bodies,
+                max_workers=threads)
+            end = time.time()
+            diff = "%.2fs" % (end - start)
+            t.add_row([
+                node_url,
+                bench_fn.__name__,
+                num_of_requests,
+                threads,
+                diff,
+            ])
+
+    click.echo("\n>Results:")
+    click.echo(t)
 
 if __name__ == '__main__':
     cli(obj={})
